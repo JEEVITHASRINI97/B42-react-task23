0e339db92fd61b5430d741980f5f9b5ca0892046
JEEVITHASRINI97 <jeevithasrinivasan97@gmail.com>
2023-01-24 20:36:50 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {PriceCard} from './PriceCard'
 
 function App() {
+
+  const plans=[
+    {
+      category: "FREE",
+      price: "0",
+      features:{
+        noOfUsers: "✔ Single User",
+        storage: "✔ 5GB Storage",
+        noOfPubProj: "✔ Unlimited Public Projects",
+        access: "✔ Community Access",
+        noOfPriProj: "❌ Unlimited Private Projects",
+        phone: "❌ Dedicated Phone Support",
+        subDomain: "❌ Free Subdomain",
+        report: "❌ Monthly Status Report"
+      }
+    },
+    {
+      category: "PLUS",
+      price: "9",
+      features:{
+        noOfUsers: "✔ 5 Users",
+        storage: "✔ 50GB Storage",
+        noOfPubProj: "✔ Unlimited Public Projects",
+        access: "✔ Community Access",
+        noOfPriProj: "✔ Unlimited Private Projects",
+        phone: "✔ Dedicated Phone Support",
+        subDomain: "✔ Free Subdomain",
+        report: "❌ Monthly Status Report"
+      }
+    },
+    {
+      category: "PRO",
+      price: "49",
+      features:{
+        noOfUsers: "✔ Unlimited Users",
+        storage: "✔ 150GB Storage",
+        noOfPubProj: "✔ Unlimited Public Projects",
+        access: "✔ Community Access",
+        noOfPriProj: "✔ Unlimited Private Projects",
+        phone: "✔ Dedicated Phone Support",
+        subDomain: "✔ Free Subdomain",
+        report: "✔ Monthly Status Report"
+      }
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PriceCard plans={plans} />
     </div>
   );
 }
